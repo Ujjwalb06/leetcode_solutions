1f5fa1af0524e326e08696f78253cfe3220bfe7f
UJJWAL BHARGAVA <ujjwalbhargava0614@gmail.com>
2026-07-12 23:49:00 +0530

    Time: 13 ms (80.63%), Space: 46.3 MB (97.15%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -2,7 +2,9 @@ class Solution {
     public String removeDuplicates(String s) {
         StringBuilder sb = new StringBuilder();
         
-        for(char ch : s.toCharArray()){
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+
             if(sb.length() > 0 && sb.charAt(sb.length() - 1) == ch){
                 sb.deleteCharAt(sb.length() - 1);
             }
